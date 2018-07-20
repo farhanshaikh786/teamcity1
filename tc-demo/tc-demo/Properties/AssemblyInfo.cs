bae214ec06feb03d3f30c1a00cdabe4a2dc9a43a
farhan shaikh <shaikh.farhan2009@gmail.com>
2018-07-20 17:38:38 +0100

    addded project

---
+++ tc-demo/tc-demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tc-demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amazon.com")]
+[assembly: AssemblyProduct("tc-demo")]
+[assembly: AssemblyCopyright("Copyright © Amazon.com 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d925fb69-1d79-4710-a4a7-d329e8a6cc9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
